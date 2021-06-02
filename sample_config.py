f48f26cafee9dbd29a75a13f24e20c71094fa2e0
Divu <70699947+imdivu@users.noreply.github.com>
2021-06-02 13:47:41 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,9 +5,13 @@ if HEROKU:
     from os import environ
 
     TOKEN = environ["TOKEN"]
-    ARQ_KEY = environ["ARQ_API_KEY"]
+    ARQ_KEY = environ["ARQ_KEY"]
+    API_ID = int(os.environ.get('API_ID'))
+    API_HASH = os.environ.get('API_HASH')
 
 # NOTE Fill this if you are not deploying on heroku.
 else:
-    TOKEN = "Insert Bot_Token Here"
+    TOKEN = "Insert Bot-token Here | @Botfather"
     ARQ_KEY = "Get this from @ARQRobot"
+    API_ID = 0
+    API_HASH = "abcd568990jjj"
